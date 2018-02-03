f272ee2ba0eb85707ed004b2bcddb5e25e4aab63
Maximal <maxim@infofree.ru>
2018-02-03 17:19:59 +0500

    interface

---
+++ src/interface/script.ts
@@ -0,0 +1,26 @@
+interface Vehicle {
+    drive(): any;
+}
+
+class Car implements Vehicle {
+    constructor(private wheels: number) {}
+
+    drive(): void {
+        document.write(`The car drives with ${this.wheels} wheels <br />`);
+    }
+}
+
+class Bicycle implements Vehicle {
+    constructor(private wheels: number) {}
+
+    drive(): void {
+        document.write(`The bicycle drives with ${this.wheels} wheels <br />`);
+    }
+}
+
+const car = new Car(4);
+const bike = new Bicycle(2);
+
+car.drive();
+bike.drive();
+
